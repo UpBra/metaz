d7792c0e6c204be0cc739a8855aaa11ce0b1143b
Blair <blair.replogle@gmail.com>
2017-06-17 19:27:45 -0600

    implement textDidEndEditing on NSTableView to move to next cell on return or tab input

--- App/src/ChaptersTableView.m
+++ App/src/ChaptersTableView.m
@@ -121,7 +121,58 @@
     return bestType != nil;
 }
 
+- (void)textDidEndEditing:(NSNotification *)notification
+{
+	NSInteger editedColumn = [self editedColumn];
+	NSInteger editedRow = [self editedRow];
+	NSInteger lastRow = [self numberOfRows];
+	NSInteger lastCol = [self numberOfColumns];
+	NSDictionary *userInfo = [notification userInfo];
+	int textMovement = [(NSNumber *)[userInfo valueForKey:@"NSTextMovement"] intValue];
+	[super textDidEndEditing: notification];
+	
+	if (textMovement == NSTabTextMovement)
+	{
+		if (editedColumn != lastCol - 1 )
+		{
+			[self selectRowIndexes:[NSIndexSet indexSetWithIndex:editedRow] byExtendingSelection:NO];
+			[self editColumn: editedColumn+1 row: editedRow withEvent: nil select: YES];
+		}
+		else
+		{
+			if (editedRow !=lastRow-1)
+			{
+				[self editColumn:0 row:editedRow + 1 withEvent:nil select:YES];
+			}
+			else
+			{
+				[self editColumn:0 row:0 withEvent:nil select:YES]; // Go to the first cell
+			}
+		}
+	}
+	else if (textMovement == NSReturnTextMovement)
+	{
+		if(editedRow !=lastRow-1)
+		{
+			[self selectRowIndexes:[NSIndexSet indexSetWithIndex:editedRow+1] byExtendingSelection:NO];
+			[self editColumn: editedColumn row: editedRow+1 withEvent: nil select: YES];
+		}
+		else
+		{
+			if (editedColumn !=lastCol - 1)
+			{
+				[self editColumn:editedColumn+1 row:0 withEvent:nil select:YES];
+			}
+			else
+			{
+				[self editColumn:0 row:0 withEvent:nil select:YES]; //Go to the first cell
+			}
+		}
+	}
+}
+
 #pragma mark - user interface validation
+
 - (BOOL)validateUserInterfaceItem:(id < NSValidatedUserInterfaceItem >)anItem
 {
     SEL action = [anItem action];
